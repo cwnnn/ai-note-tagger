1c7ede9697b613fda1fe3a44f0d04b4c37b67da0
cwnnn <cangs026@gmail.com>
2026-01-11 05:25:59 +0300

    used cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import tagNoteRoute from "./routes/tagNote.route.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", tagNoteRoute);
 
