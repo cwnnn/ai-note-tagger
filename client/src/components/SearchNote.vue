977f0f9faac72815d2f0385eb282a21edd82a0da
cwnnn <cangs026@gmail.com>
2026-01-12 22:12:31 +0300

    add source

---
+++ client/src/components/SearchNote.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useNotesStore } from "../stores/useNotesStore";
+
+const notesStore = useNotesStore();
+</script>
+
+<template>
+  <input
+    v-model="notesStore.searchQuery"
+    placeholder="Başlık veya etiket ara..."
+    class="px-4 py-2 h-14 w-100 rounded-full backdrop-blur-xs text-white outline-none border duration-200 hover:scale-105 focus:border-3 focus:scale-105 focus:border-(--btn-primary)"
+  />
+</template>
