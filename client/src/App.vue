e830dc0e7685ccbe5e97ee2a742c8b3bae7803c2
cwnnn <cangs026@gmail.com>
2026-01-17 01:12:42 +0300

    fixed and use router in app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,111 +1,82 @@
 <script setup>
-import { ref } from "vue";
-import { tagNote } from "./services/api";
-import NoteForm from "./components/NoteForm.vue";
-import TagButton from "./components/TagButton.vue";
-import HomeView from "./views/homeView.vue";
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
+
 import AppHeader from "./components/AppHeader/AppHeader.vue";
 import BaseButton from "./components/BaseButton/BaseButton.vue";
 import SearchNote from "./components/SearchNote.vue";
-import BaseModal from "./components/Modal/BaseModal.vue";
+import { useModalStore } from "./stores/modalStore";
 
-const tags = ref([]);
+const modalStore = useModalStore();
+
+const route = useRoute();
+
 const loading = ref(false);
 const error = ref(null);
-const isModalOpen = ref(false);
 
-const title = ref("Dışarıdan gelen başlık");
-const content = ref("Dışarıdan gelen içerik");
-
-function openCreate() {
-  selectedNote.value = null;
-  isModalOpen.value = true;
-}
-
-async function getTagAi(title, content) {
-  const note = "Title : " + title + ", Content : " + content;
-  loading.value = true;
-  error.value = null;
-  tags.value = [];
-  console.log("gönderilen", note);
-  try {
-    const result = await tagNote(note);
-    tags.value = result.data.tags;
-    console.log("taglar:", tags.value);
-  } catch (err) {
-    error.value = err.response?.data?.error || "Bir hata oluştu";
-  } finally {
-    loading.value = false;
+watch(
+  () => modalStore.isModalOpen,
+  (val) => {
+    if (!val) {
+      error.value = null;
+      loading.value = false;
+    }
   }
-}
+);
 </script>
 
 <template>
   <main>
     <AppHeader class="pb-5">
       <template #logo>
-        <a class="font-bold text-3xl">RCS Notion</a>
+        <router-link to="/">
+          <a class="font-bold text-3xl min-w-max whitespace-nowrap"
+            >RCS Notion</a
+          >
+        </router-link>
       </template>
 
-      <template #nav><SearchNote /> </template>
+      <template #nav>
+        <SearchNote class="md:ml-auto" />
+
+        <router-link
+          to="/"
+          class="links md:ml-auto"
+          exact-active-class="active"
+        >
+          Home
+        </router-link>
+        <router-link to="/archive" class="links" active-class="active"
+          >Archive</router-link
+        >
+        <router-link to="/trash" class="links" active-class="active"
+          >Trash</router-link
+        >
+      </template>
 
       <template #actions>
         <BaseButton
+          :disabled="!route.meta.showHeaderButton"
           variant="outline"
-          class="h-14 backdrop-blur-3xl"
-          @click="isModalOpen = true"
-          ><span class="">New Note</span
-          ><span class="mb-1.5 text-3xl font-light">+</span></BaseButton
+          class="h-14 min-w-max backdrop-blur-3xl"
+          @click="modalStore.openCreate"
         >
+          <span>New Note</span>
+          <span class="mb-1.5 text-3xl font-light">+</span>
+        </BaseButton>
       </template>
     </AppHeader>
 
-    <HomeView />
-
-    <BaseModal v-model="isModalOpen">
-      <NoteForm
-        v-model:title="title"
-        v-model:content="content"
-        :loading="loading"
-        @submit="content"
-        class="p-4"
-      >
-        <template #tags>
-          <div class="flex flex-wrap gap-2">
-            <TagButton
-              v-for="tag in tags"
-              :key="tag"
-              :label="tag"
-              removable
-              @click="onTagClick(tag)"
-              @remove="onTagRemove(tag)"
-            />
-
-            <TagButton>
-              <span class="text-green-300">Tag ekle +</span>
-            </TagButton>
-          </div>
-
-          <BaseButton
-            color="secondary"
-            :loading="loading"
-            :disabled="loading || !content.trim()"
-            @click="getTagAi(title, content)"
-            class="w-40 ml-auto"
-          >
-            <span>{{ loading ? "Etiketleniyor..." : "Ai Etiket" }}</span>
-          </BaseButton>
-        </template>
-        <template #footer>
-          <div class="flex flex-col items-center">
-            <BaseButton variant="outline" class="w-100">kaydet</BaseButton>
-            <p v-if="error" style="color: red" class="pt-5">
-              Bir sorun oldu, daha sonra terkar dene :(
-              {{ error }}
-            </p>
-          </div>
-        </template>
-      </NoteForm>
-    </BaseModal>
+    <router-view />
   </main>
 </template>
+<style>
+@reference "tailwindcss/theme.css";
+
+.links {
+  @apply pl-2 duration-300 hover:scale-110;
+}
+.active {
+  @apply text-white duration-400;
+}
+</style>
