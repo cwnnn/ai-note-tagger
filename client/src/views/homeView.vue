80ee9d8cb4d02c914b6be386959c7971384307b8
cwnnn <cangs026@gmail.com>
2026-01-17 01:10:58 +0300

    refactor homeView: update note rendering to use active status and enhance modal functionality with tag management

--- client/src/views/homeView.vue
+++ client/src/views/homeView.vue
@@ -4,36 +4,261 @@
       class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4"
     >
       <div
-        v-for="note in notesStore.filteredNotes"
+        v-for="note in notesStore.notesByStatus.active"
         :key="note.id"
         class="mb-4 break-inside-avoid rounded-2xl border border-white/30 p-4 shadow-md duration-200 hover:scale-105"
+        @click="handleClick(note.id)"
       >
         <h3 class="mb-2 text-lg font-semibold">{{ note.title }}</h3>
         <p class="mb-3 text-sm text-white/60">
           {{ note.content }}
         </p>
         <div class="flex flex-wrap gap-2">
-          <TagButton
-            v-for="tag in note.tags"
-            :key="tag"
-            :label="tag"
-            @click="onTagClick(tag)"
-            @remove="onTagRemove(tag)"
-          />
+          <TagButton v-for="tag in note.tags" :key="tag" :label="tag" />
         </div>
       </div>
     </div>
   </div>
+  <BaseModal v-model="modalStore.isModalOpen">
+    <NoteForm
+      v-model:title="modalStore.form.title"
+      v-model:content="modalStore.form.content"
+      :date="formattedDate"
+      :loading="loading"
+      class="p-4"
+    >
+      <template #header
+        ><button
+          @click="handleArchive"
+          class="duration-300 hover:scale-110 cursor-pointer"
+        >
+          <img
+            class="w-10 h-10 invert"
+            src="../../public/archive.png"
+            alt="archive"
+          /></button
+      ></template>
+      <template #tags>
+        <div class="flex flex-wrap gap-2">
+          <TagButton
+            v-for="tag in modalStore.form.tags"
+            :key="tag"
+            :label="tag"
+            removable
+            @click="onTagClick(tag)"
+            @remove="onTagRemove(tag)"
+          />
+
+          <TagButton>
+            <input
+              type="text"
+              placeholder="Add Tag"
+              v-model="newTag"
+              @keyup.enter="addTag"
+              :size="Math.max(newTag.length, 6)"
+              class="outline-none"
+            />
+            <span
+              v-if="!newTag == ''"
+              class="cursor-pointer text-base duration-300 hover:text-green-300"
+              @click.stop="addTag"
+            >
+              +
+            </span>
+          </TagButton>
+        </div>
+
+        <BaseButton
+          color="secondary"
+          :loading="loading"
+          :disabled="
+            loading ||
+            (!modalStore.form.title.trim() && !modalStore.form.content.trim())
+          "
+          @click="getTagAi"
+          class="w-40 ml-auto"
+        >
+          <span>{{ loading ? "Etiketleniyor..." : "Ai Tag" }}</span>
+        </BaseButton>
+      </template>
+
+      <template #footer>
+        <div
+          class="flex flex-row"
+          :class="modalStore.isCreate ? 'justify-center' : 'justify-between'"
+        >
+          <BaseButton
+            v-if="!modalStore.isCreate"
+            variant="outline"
+            color="danger"
+            class="w-70"
+            @click="trashNoteForm"
+          >
+            Move to Trash
+          </BaseButton>
+
+          <BaseButton
+            variant="outline"
+            :class="modalStore.isCreate ? 'w-100' : 'w-70'"
+            @click="modalStore.isCreate ? addNewNoteForm() : EditNoteForm()"
+            :disabled="
+              loading ||
+              (!modalStore.form.title.trim() && !modalStore.form.content.trim())
+            "
+          >
+            {{ modalStore.isCreate ? "Save" : "Edit" }}
+          </BaseButton>
+
+          <p v-if="error" style="color: red" class="pt-5">
+            Bir sorun oldu, daha sonra tekrar dene :(
+            {{ error }}
+          </p>
+        </div>
+      </template>
+    </NoteForm>
+  </BaseModal>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
+import { tagNote } from "../services/api";
 import { useNotesStore } from "../stores/useNotesStore";
+import { useModalStore } from "../stores/modalStore";
 import TagButton from "../components/TagButton.vue";
+import BaseModal from "../components/Modal/BaseModal.vue";
+import NoteForm from "../components/NoteForm.vue";
+import BaseButton from "../components/BaseButton/BaseButton.vue";
 
 const notesStore = useNotesStore();
+const modalStore = useModalStore();
 
-// Yeni eklenen json en üste gelsin diye ters çeviriyoruz
-const notes = computed(() => [...notesData].reverse());
+const loading = ref(false);
+const error = ref(null);
+
+onMounted(() => {
+  notesStore.fetchNotes();
+});
+
+function handleClick(noteId) {
+  const note = notesStore.notes.find((n) => n.id === noteId);
+  if (!note) return;
+
+  modalStore.openEdit(note);
+}
+const formattedDate = computed(() => {
+  const raw = modalStore.form.updatedAt;
+  if (!raw) return "";
+  const d = new Date(raw);
+  return d.toLocaleString("tr-TR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+});
+
+async function addNewNoteForm() {
+  if (
+    modalStore.form.content.trim() == "" &&
+    modalStore.form.title.trim() == ""
+  )
+    return;
+
+  const note = {
+    title: modalStore.form.title,
+    content: modalStore.form.content,
+    tags: [...modalStore.form.tags],
+  };
+  const newNoteId = await notesStore.createNote(note);
+
+  if (!newNoteId) return;
+
+  modalStore.close();
+  return newNoteId;
+}
+
+/* Notu düzenleme */
+function EditNoteForm() {
+  if (!modalStore.activeNoteId) return;
+  const note = {
+    title: modalStore.form.title,
+    content: modalStore.form.content,
+    tags: [...modalStore.form.tags],
+  };
+  notesStore.editNote(modalStore.activeNoteId, note);
+  modalStore.close();
+}
+
+function trashNoteForm() {
+  if (!modalStore.activeNoteId) return;
+  const note = {
+    title: modalStore.form.title,
+    content: modalStore.form.content,
+    tags: [...modalStore.form.tags],
+  };
+  notesStore.editNote(modalStore.activeNoteId, note);
+  notesStore.trash(modalStore.activeNoteId);
+  modalStore.close();
+}
+
+function archiveNoteForm() {
+  if (!modalStore.activeNoteId) return;
+  notesStore.archive(modalStore.activeNoteId);
+  modalStore.close();
+}
+
+async function getTagAi() {
+  const payload =
+    "Title : " +
+    modalStore.form.title +
+    ", Content : " +
+    modalStore.form.content;
+  loading.value = true;
+  error.value = null;
+  try {
+    const result = await tagNote(payload);
+    // backend dönen şekle göre ayarla; örnek: result.data.tags
+    modalStore.form.tags = result.data?.tags ?? [];
+  } catch (err) {
+    error.value = err.response?.data?.error || "Bir hata oluştu";
+  } finally {
+    loading.value = false;
+  }
+}
+
+const newTag = ref("");
+
+function addTag() {
+  if (!newTag.value.trim()) return;
+
+  modalStore.form.tags.push(newTag.value.trim());
+  newTag.value = ""; // input temizlensin
+}
+
+function onTagRemove(tag) {
+  modalStore.form.tags = modalStore.form.tags.filter((t) => t !== tag);
+}
+
+async function handleArchive() {
+  if (
+    modalStore.form.content.trim() == "" &&
+    modalStore.form.title.trim() == ""
+  )
+    return;
+  if (modalStore.isCreate) {
+    const newNoteId = await addNewNoteForm();
+    modalStore.activeNoteId = newNoteId;
+  } else {
+    const note = {
+      title: modalStore.form.title,
+      content: modalStore.form.content,
+      tags: [...modalStore.form.tags],
+    };
+    notesStore.editNote(modalStore.activeNoteId, note);
+  }
+
+  archiveNoteForm();
+}
 </script>
